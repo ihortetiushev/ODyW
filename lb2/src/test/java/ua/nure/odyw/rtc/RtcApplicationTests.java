57996dbbeed4d56fb20fe1081583cb466ccb6377
Ihor Tetiushev <ihor.tetiushev@gmail.com>
2024-11-21 00:22:41 +0200

    lb 2 init

---
+++ lb2/src/test/java/ua/nure/odyw/rtc/RtcApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.odyw.rtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
